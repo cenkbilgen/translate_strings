938a8bd567da72c1bd03b6bb5e764b769fdee1e5
cenk <cenk.bilgen@gmail.com>
2025-01-07 16:32:04 -0500

    update keychain package

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/apple/swift-algorithms.git", from: "1.2.0"),
         .package(url: "https://github.com/cenkbilgen/StringsCatalogKit.git", from: "1.0.0"),
-        .package(url: "https://github.com/cenkbilgen/KeychainSimple.git", from: "1.0.0")
+        .package(url: "https://github.com/cenkbilgen/KeychainSimple.git", from: "2.0.0")
     ],
     targets: [
         .executableTarget(
